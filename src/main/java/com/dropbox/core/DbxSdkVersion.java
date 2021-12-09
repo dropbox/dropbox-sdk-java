9308a661bf2db98acd4034806b9d3d80a7073cef
David Chang <changd@dropbox.com>
2021-12-09 15:50:43 -0800

    Prepare for release 5.1.0

--- src/main/java/com/dropbox/core/DbxSdkVersion.java
+++ src/main/java/com/dropbox/core/DbxSdkVersion.java
@@ -12,6 +12,6 @@ public class DbxSdkVersion
     // https://github.com/dropbox/dropbox-sdk-java/issues/357
     private static String loadVersion()
     {
-        return "5.0.0-SNAPSHOT";
+        return "5.1.0";
     }
 }
