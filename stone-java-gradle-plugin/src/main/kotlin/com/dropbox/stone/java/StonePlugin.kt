c9f705e2c07f24066d371e469ac4e01161b20b54
Jay Palacio <jaypalacio@gmail.com>
2023-01-22 09:00:50 -0500

    config cache v1
    fix proguard config cache

--- stone-java-gradle-plugin/src/main/kotlin/com/dropbox/stone/java/StonePlugin.kt
+++ stone-java-gradle-plugin/src/main/kotlin/com/dropbox/stone/java/StonePlugin.kt
@@ -6,12 +6,11 @@ import org.gradle.api.Task
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SourceSet
-import org.gradle.api.tasks.TaskProvider
 
 /**
- * Android Application Convention Plugin
+ * Stone Plugin
  *
- * We'll apply all of Dropbox's Custom Config here.
+ * Allows us to apply a Stone Plugin and utilize StoneTask.
  */
 public class StonePlugin : Plugin<Project> {
 
@@ -37,50 +36,47 @@ public class StonePlugin : Plugin<Project> {
         } else {
             "generate${sourceSet.name.capitalize()}Stone"
         }
-        val stoneTaskProvider: TaskProvider<StoneTask> = project.tasks.register(taskName, StoneTask::class.java)
-        stoneTaskProvider.get().also { stoneTask ->
-            stoneTask.description = "Generate Stone Java source files for ${sourceSet.name}."
+
+        project.tasks.register(taskName, StoneTask::class.java){
+            description = "Generate Stone Java source files for ${sourceSet.name}."
 
             val routeWhitelistFilterPropName = "com.dropbox.api.${sourceSet.name}.routeWhitelistFilter"
             val routeWhitelistFilterValue: String? = project.properties[routeWhitelistFilterPropName] as String?
             routeWhitelistFilterValue?.let {
-                stoneTask.routeWhitelistFilter(it)
+                routeWhitelistFilter(it)
             }
 
             val specDirPropName = "com.dropbox.api.${sourceSet.name}.specDir"
             val specDirPropNameValue: String? = project.properties[specDirPropName] as String?
             val mySpecDir: String = specDirPropNameValue ?: "src/${sourceSet.name}/stone"
 
-            stoneTask.specDir(mySpecDir)
-            stoneTask.generatorDir("${project.projectDir.absoluteFile}/generator/java")
-            stoneTask.stoneDir("stone")
-            stoneTask.pythonCommand("python")
-            stoneTask.outputDir("${project.buildDir}/generated/source/stone/${sourceSet.name}")
-            stoneTask.inputs
-                .dir { project.fileTree(stoneTask.getGeneratorDir()) { filter { !it.name.endsWith(".pyc") } } }
+            specDir(mySpecDir)
+            generatorDir("${project.projectDir.absoluteFile}/generator/java")
+            stoneDir("stone")
+            pythonCommand("python")
+            outputDir("${project.buildDir}/generated/source/stone/${sourceSet.name}")
+            inputs
+                .dir { project.fileTree(this.getGeneratorDir()) { filter { !it.name.endsWith(".pyc") } } }
                 .withPropertyName("stone")
                 .withPathSensitivity(PathSensitivity.RELATIVE)
 
-            val specDir = stoneTask.getSpecDir().get()
-            stoneTask.inputs
-                .dir(stoneTask.getSpecFiles(project.objects, specDir))
+            val specDir = this.getSpecDir().get()
+            inputs
+                .dir(this.getSpecFiles(project.objects, specDir))
                 .withPathSensitivity(PathSensitivity.RELATIVE)
                 .withPropertyName("stoneSpec")
                 .skipWhenEmpty(true)
-            stoneTask.outputs
-                .dir { stoneTask.getOutputDir() }
+            outputs
+                .dir { this.getOutputDir() }
                 .withPropertyName("generatedStone")
-            stoneTask.outputs.cacheIf { true }
+            outputs.cacheIf { true }
 
             val compileJava: Task = project.tasks.getByName(sourceSet.getCompileTaskName("java"))
-            compileJava.dependsOn(stoneTask)
+            compileJava.dependsOn(project.tasks.named(taskName))
             val compileKotlin: Task = project.tasks.getByName(sourceSet.getCompileTaskName("kotlin"))
-            compileKotlin.dependsOn(stoneTask)
+            compileKotlin.dependsOn(project.tasks.named(taskName))
 
-            project.afterEvaluate {
-                // Must run afterEvaluate so we can honor any output directory specified in user's config
-                sourceSet.java.srcDir("${stoneTask.getOutputDir().get()}/src")
-            }
+            sourceSet.java.srcDir("${getOutputDir().get()}/src")
         }
     }
 }
